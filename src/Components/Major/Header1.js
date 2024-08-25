0c036df09bfb6d4455117ae9f0efad8a4bcecb9e
MembaDavid <davidmemba2@gmail.com>
2024-08-26 00:09:10 +0300

    Initial Commit

---
+++ src/Components/Major/Header1.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header1() {
+  return (
+    <div>
+	 <div>
+		
+	 </div>
+	 <div>
+
+	 </div>
+	 <div>
+		
+	 </div>
+    </div>
+  )
+}
+
+export default Header1
