55e7337215262cf9048506c64e4c24cc0a8f25b0
MembaDavid <davidmemba2@gmail.com>
2024-08-28 11:58:54 +0300

    Debug I

---
+++ src/app/culturalmapping/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+	 Cultural Mapping
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/earlymobilityandmigration/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+	 Early Mobility and Migration
+    </div>
+  )
+}
+
+export default page
