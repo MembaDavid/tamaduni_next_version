79f6c43ab6b39fbf647bf3713d16720242790197
MembaDavid <davidmemba2@gmail.com>
2024-08-28 11:28:21 +0300

    Routing

--- src/Components/Major/Header1.js
+++ src/Components/Major/Header1.js
@@ -1,19 +1,1 @@
-import React from 'react'
 
-function Header1() {
-  return (
-    <div>
-	 <div>
-		
-	 </div>
-	 <div>
-
-	 </div>
-	 <div>
-		
-	 </div>
-    </div>
-  )
-}
-
-export default Header1
