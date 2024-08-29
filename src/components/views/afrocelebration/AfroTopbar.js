475e95f9582b0e1b01c4c34b57c069adb1ed157d
eddiemuhoro <eddiemuhoro@gmail.com>
2024-08-29 16:48:31 +0300

    chore: Update AfroTopbar component styling and structure

--- src/components/views/afrocelebration/AfroTopbar.js
+++ src/components/views/afrocelebration/AfroTopbar.js
@@ -1,48 +1,50 @@
-import React from 'react'
-import { FaBars, FaSearch } from 'react-icons/fa'
+import React from "react";
+import { FaBars, FaSearch } from "react-icons/fa";
 
 const AfroTopbar = () => {
-    const menuItems = [
-        {
-            name: 'Museum'
-        },
-        {
-            name: 'Menu Item'
-        },
-        {
-            name: 'Menu Item'
-        },
-      
-    ]
+  const menuItems = [
+    {
+      name: "Museum",
+    },
+    {
+      name: "Menu Item",
+    },
+    {
+      name: "Menu Item",
+    },
+  ];
 
   return (
     <div className=" py-2 px-4 flex items-center justify-between">
       {/* Search Section */}
       <div className="flex items-center bg-gray-100 rounded-full px-4 py-2">
         <FaBars className="text-gray-600 mr-3" />
-        <input 
-          type="text" 
-          placeholder="Country i.e Kenya" 
-          className="bg-transparent outline-none text-gray-600 placeholder-gray-500 w-full" 
+        <input
+          type="text"
+          placeholder="Country i.e Kenya"
+          className="bg-transparent text-sm outline-none text-gray-600 placeholder-gray-500 w-full"
         />
         <FaSearch className="text-gray-600 ml-3" />
       </div>
 
       <div className="flex space-x-2 items-center">
-        <select className="bg-gray-100 text-gray-600 py-2 px-3 rounded-md border border-gray-400 focus:outline-none focus:border-black">
-          <option>Heritage Places</option>
+        <select className="bg-gray-100 text-sm text-gray-600 py-2 px-3 rounded-md border border-gray-400 focus:outline-none focus:border-black">
+          <option className="text-sm">Heritage Places</option>
           <option>Option 1</option>
           <option>Option 2</option>
         </select>
 
-        {
-            menuItems.map((item, index) => (
-                <button key={index} className="bg-black text-white py-2 px-2 rounded-md">{item.name}</button>
-            ))
-        }
+        {menuItems.map((item, index) => (
+          <button
+            key={index}
+            className="bg-black text-sm text-white py-2 px-2 rounded-md"
+          >
+            {item.name}
+          </button>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AfroTopbar
+export default AfroTopbar;
