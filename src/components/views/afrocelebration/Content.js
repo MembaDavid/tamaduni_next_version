7f59adb969a3c94e79907d310ca58919e8ce9fd7
eddiemuhoro <eddiemuhoro@gmail.com>
2024-08-30 14:09:16 +0300

    refactor: Optimize dynamic imports in ContentSlider component

--- src/components/views/afrocelebration/Content.js
+++ src/components/views/afrocelebration/Content.js
@@ -1,19 +1,21 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import dynamic from "next/dynamic";
 import {
   FaGlobe,
   FaMapMarkerAlt,
-  FaAngleLeft,
-  FaAngleRight,
   FaGalacticRepublic,
 } from "react-icons/fa";
-import Gallery from "./Gallery";
-import Map from "./Map";
-import List from "./list/List";
+
+// Dynamic imports with SSR disabled
+const List = dynamic(() => import("./list/List"), { ssr: false });
+const Gallery = dynamic(() => import("./Gallery"), { ssr: false });
+const Map = dynamic(() => import("./Map"), { ssr: false });
 
 const ContentSlider = () => {
   const [selectedTab, setSelectedTab] = useState("list");
+
   const handleTabClick = (tab) => {
     setSelectedTab(tab);
   };
@@ -22,15 +24,15 @@ const ContentSlider = () => {
     <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
       {/* Left Section */}
       <div className="w-full md:w-1/4 flex flex-col items-center">
-      <div className="sm:w-1/2 md:w-full relative w-full ">
-        <Image
-          src="/afrocelebration/kenya.png"
-          alt="Kenya Flags"
-          layout="responsive"
-          width={100}
-          height={100}
-          className="rounded-lg mb-4"
-        />
+        <div className="sm:w-1/2 md:w-full relative w-full">
+          <Image
+            src="/afrocelebration/kenya.png"
+            alt="Kenya Flags"
+            layout="responsive"
+            width={100}
+            height={100}
+            className="rounded-lg mb-4"
+          />
         </div>
         <div className="bg-gray-200 p-4 rounded-lg w-full flex flex-col items-center">
           <h3 className="text-sm font-bold mb-2">Synopsis</h3>
