df1ab96eab25e8e19d4db72e3fa39b826349fb10
eddiemuhoro <eddiemuhoro@gmail.com>
2024-08-30 13:24:53 +0300

    refactor: Update List component import path in ContentSlider

--- src/components/views/afrocelebration/Content.js
+++ src/components/views/afrocelebration/Content.js
@@ -10,26 +10,29 @@ import {
 } from "react-icons/fa";
 import Gallery from "./Gallery";
 import Map from "./Map";
-import List from "./List";
+import List from "./list/List";
 
 const ContentSlider = () => {
-  const [selectedTab, setSelectedTab] = useState("gallery");
+  const [selectedTab, setSelectedTab] = useState("list");
   const handleTabClick = (tab) => {
     setSelectedTab(tab);
   };
+
   return (
-    <div className="bg-white shadow-lg rounded-lg p-4 flex space-x-4">
+    <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
       {/* Left Section */}
-      <div className="w-1/4 flex flex-col items-center">
+      <div className="w-full md:w-1/4 flex flex-col items-center">
+      <div className="sm:w-1/2 md:w-full relative w-full ">
         <Image
           src="/afrocelebration/kenya.png"
-          alt="Kenya Flag"
+          alt="Kenya Flags"
           layout="responsive"
           width={100}
           height={100}
           className="rounded-lg mb-4"
         />
-        <div className="bg-gray-200 p-4 rounded-lg h-full">
+        </div>
+        <div className="bg-gray-200 p-4 rounded-lg w-full flex flex-col items-center">
           <h3 className="text-sm font-bold mb-2">Synopsis</h3>
           <p className="text-xs text-gray-600">
             Institution that collects, preserves, displays, and interprets
@@ -41,11 +44,11 @@ const ContentSlider = () => {
       </div>
 
       {/* Right Section */}
-      <div className="w-3/4 pl-4">
+      <div className="w-full md:w-3/4 pl-0 md:pl-4">
         {/* Icons Row */}
         <div className="flex justify-between items-center mb-2">
           <div className="flex space-x-2">
-          <FaGalacticRepublic
+            <FaGalacticRepublic
               size={24}
               className={`cursor-pointer p-1 ${
                 selectedTab === "gallery"
@@ -72,18 +75,20 @@ const ContentSlider = () => {
               }`}
               onClick={() => handleTabClick("map")}
             />
-          
           </div>
-          <div className="mt-2">
+          <div className="hidden md:block mt-2">
             <h2 className="text-lg font-bold">Film Industry</h2>
             <p className="text-sm text-gray-600 italic">Film Directing</p>
           </div>
-          <span className="text-xs text-gray-600">1/3</span>
+          <span className="hidden md:block text-xs text-gray-600">1/3</span>
         </div>
 
-        {selectedTab === "gallery" && <Gallery />}
-        {selectedTab === "map" && <List />}
-        {selectedTab === "list" && <Map />}
+        {/* Dynamic Content Area */}
+        <div className="mt-2">
+          {selectedTab === "gallery" && <Gallery />}
+          {selectedTab === "list" && <List />}
+          {selectedTab === "map" && <Map />}
+        </div>
       </div>
     </div>
   );
