7acea628c243592b1bc8333dbbb127f92fdb3009
eddiemuhoro <eddiemuhoro@gmail.com>
2024-09-02 11:25:02 +0300

    added background image and fix font style

--- src/components/hero-section/Hero.js
+++ src/components/hero-section/Hero.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { Almendra } from "next/font/google";
+
+const almandra = Almendra({
+  subsets: ["latin"],
+  weight: "400", // Specify the weight if needed (e.g., '400')
+  display: "swap", // Optional: control how the font is displayed
+});
 
 const Hero = () => {
   return (
@@ -7,11 +14,13 @@ const Hero = () => {
         <div className="absolute inset-0 bg-[#1F007DC2]"></div>
 
         <div className="text-center z-10 lg:px-[15%] md:px-[10%] sm:px-5">
-          <p className="text-white text-2xl font-almendra">
+          <p
+            className={`text-white text-xl md:text-2xl font-almendra p-4 ${almandra.className}`}
+          >
             Africanacity is the rhythm of resilience, the melody of diversity,
-            and the harmony of strength. It&#39;s the celebration of a continent&#39;s
-            spirit, echoing through time with the vibrant beats of innovation,
-            creativity, and unity
+            and the harmony of strength. It&#39;s the celebration of a
+            continent&#39;s spirit, echoing through time with the vibrant beats
+            of innovation, creativity, and unity
           </p>
         </div>
       </div>
