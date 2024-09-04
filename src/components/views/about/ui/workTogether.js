3ffab17fc8f878f20f0b50573e9fdbf6470ac576
eddiemuhoro <eddiemuhoro@gmail.com>
2024-09-05 01:54:31 +0300

    refactor: Improve styling and responsiveness on About page

--- src/components/views/about/ui/workTogether.js
+++ src/components/views/about/ui/workTogether.js
@@ -3,12 +3,12 @@ import React from "react";
 
 const WorkTogether = () => {
   return (
-    <section className="bg-gray-800 text-white py-16 text-center">
+    <section className="bg-gray-800 text-white py-16 text-center px-5">
       <h2 className="text-3xl font-bold mb-8">Let's Work Together</h2>
       <p className="text-lg mb-6">
-        Whether you’re looking for a partner in innovation or to join a team
-        that values creativity, collaboration, and community, we’d love to hear
-        from you!
+        Whether you&#39;re looking for a partner in innovation or to join a team
+        that values creativity, collaboration, and community, we&#39;d love to
+        hear from you!
       </p>
       <Link
         className="bg-white text-gray-800 px-6 py-3 rounded-lg text-lg hover:bg-gray-200 transition"
