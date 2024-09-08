2f7096537fd70b3eb1a3b46fb695d6878668b0ef
MembaDavid <davidmemba2@gmail.com>
2024-09-08 14:43:48 +0300

    Update component files

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 function main() {
+  function count_even_numbers(){
+  }
   return (
     <div className='flex justify-center items-center font-serif text-4xl font-bold'>
       This is the home page
