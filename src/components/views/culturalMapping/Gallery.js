6e0bec7044eecd85d17e7b569e55d88f8e9e9dd2
eddiemuhoro <eddiemuhoro@gmail.com>
2024-09-17 11:52:37 +0300

    Refactor Gallery component to add image slideshow functionality

--- src/components/views/culturalMapping/Gallery.js
+++ src/components/views/culturalMapping/Gallery.js
@@ -1,22 +1,68 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
+const images = [
+  "https://images.unsplash.com/photo-1515318650024-02fe7e49c5b9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1471341971476-ae15ff5dd4ea?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1515318650024-02fe7e49c5b9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
 const Gallery = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isFading, setIsFading] = useState(false);
+
+  const handleNext = () => {
+    setIsFading(true);
+    setTimeout(() => {
+      if (currentIndex < images.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        setCurrentIndex(0);
+      }
+      setIsFading(false);
+    }, 300);
+  };
+
+  const handlePrev = () => {
+    setIsFading(true);
+    setTimeout(() => {
+      if (currentIndex > 0) {
+        setCurrentIndex(currentIndex - 1);
+      } else {
+        setCurrentIndex(images.length - 1);
+      }
+      setIsFading(false);
+    }, 300);
+  };
+
   return (
     <div className="relative w-full h-full">
-      <Image
-        src="https://images.unsplash.com/photo-1515318650024-02fe7e49c5b9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Main Content"
-        layout="fill"
-        objectFit="cover" // or "contain" depending on your desired behavior
-        className="rounded-lg"
-      />
-      {/* Navigation Arrows */}
-      <button className="absolute left-0 top-1/2 transform -translate-y-1/2 hover:bg-gray-200 p-1 rounded-full transition duration-300">
+      <div
+        className={`w-full h-full relative transition-opacity duration-300 ${
+          isFading ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        <Image
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex}`}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+        />
+      </div>
+
+      <button
+        onClick={handlePrev}
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 hover:bg-gray-200 p-1 rounded-full transition duration-300"
+      >
         <FaAngleLeft className="text-gray-600" />
       </button>
-      <button className="absolute right-0 top-1/2 transform -translate-y-1/2 hover:bg-gray-200 p-1 rounded-full transition duration-300">
+
+      <button
+        onClick={handleNext}
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 hover:bg-gray-200 p-1 rounded-full transition duration-300"
+      >
         <FaAngleRight className="text-gray-600" />
       </button>
     </div>
