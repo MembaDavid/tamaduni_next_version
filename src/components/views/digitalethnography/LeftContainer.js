031b0596a241c9f3599e5695e55edba697df2a70
eddiemuhoro <eddiemuhoro@gmail.com>
2024-09-24 22:34:56 +0300

    Refactor LeftContainer component styles for Digital Ethnography page

--- src/components/views/digitalethnography/LeftContainer.js
+++ src/components/views/digitalethnography/LeftContainer.js
@@ -5,7 +5,7 @@ const LeftContainer = () => {
     <div className="flex flex-col gap-4  w-full mb-4">
       <section className="flex flex-col gap-4 items-center bg-gray-200 p-6 rounded-lg">
         <h1 className="text-2xl font-semibold">GROUP</h1>
-        <h2 className="text-xl font-semibold">BANTU</h2>
+        <h2 className=" font-semibold">BANTU</h2>
         <p className="text-gray-600 text-center">
           Bantu people are an indigenous group of people in Africa. Initially
           located in central Africa, they later spread to other parts of the
@@ -13,10 +13,10 @@ const LeftContainer = () => {
           found in Eastern, Central, and Southern Africa
         </p>
       </section>
-      <section className="flex flex-col gap-4 bg-gray-200 p-6 rounded-lg">
+      <section className="flex flex-col gap-4 items-center bg-gray-200 p-6 rounded-lg">
         <h1 className="text-2xl font-semibold">Timeline Synosis</h1>
-        <h2 className="text-xl font-semibold">3000 BCE</h2>
-        <p className="text-gray-600">
+        <h2 className=" font-semibold">3000 BCE</h2>
+        <p className="text-gray-600 text-center">
           The drying of the Sahara around 300B BCE engendered population
           redistribution that occurred over centuries. Retreat of rainforest in
           favor of patches of savannah in parts of West Central facilitated the
