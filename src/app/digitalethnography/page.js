95a3e0eab9e15e8985bfc91f3cbd0420b79aef1a
eddiemuhoro <eddiemuhoro@gmail.com>
2024-09-24 22:31:36 +0300

    Refactor page component to include Digital Ethnography topbar and content

--- src/app/digitalethnography/page.js
+++ src/app/digitalethnography/page.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import DEContent from "@/components/views/digitalethnography/Content";
+import DETopbar from "@/components/views/digitalethnography/DETopbar";
+import React from "react";
 
 function page() {
   return (
     <div>
-	 Digital Ethnography
+      <DETopbar />
+      <DEContent />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
