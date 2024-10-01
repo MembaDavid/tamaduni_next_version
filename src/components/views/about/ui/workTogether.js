b193d0a0b579ed9bd0418b13fbd4a1827400e2d5
eddiemuhoro <eddiemuhoro@gmail.com>
2024-10-01 21:22:27 +0300

    Fix HTML encoding in workTogether.js

--- src/components/views/about/ui/workTogether.js
+++ src/components/views/about/ui/workTogether.js
@@ -4,7 +4,7 @@ import React from "react";
 const WorkTogether = () => {
   return (
     <section className="bg-gray-800 text-white py-16 text-center px-5">
-      <h2 className="text-3xl font-bold mb-8">Let's Work Together</h2>
+      <h2 className="text-3xl font-bold mb-8">Let&#39;s Work Together</h2>
       <p className="text-lg mb-6">
         Whether you&#39;re looking for a partner in innovation or to join a team
         that values creativity, collaboration, and community, we&#39;d love to
