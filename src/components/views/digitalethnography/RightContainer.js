f0600f0024d8c2816e5ee43491983b7372f8c3e8
eddiemuhoro <eddiemuhoro@gmail.com>
2024-12-01 14:16:53 +0300

    refactor Map and RightContainer components for improved structure and styling

--- src/components/views/digitalethnography/RightContainer.js
+++ src/components/views/digitalethnography/RightContainer.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { FaGalacticRepublic, FaGlobe, FaMapMarkerAlt } from "react-icons/fa";
 import MapComponent from "../culturalMapping/Map";
 import Map from "./map/Map";
+import Gallery from "../culturalMapping/Gallery";
 
 const RightContainer = () => {
   const [selectedTab, setSelectedTab] = useState("gallery");
@@ -11,14 +12,14 @@ const RightContainer = () => {
   };
 
   return (
-    <section className="w-full">
+    <section className="w-full p-2">
       <div className="flex justify-between items-center mb-2">
-        <div className=" mt-2">
-          <h1 className="text-lg font-bold">Natural Catastrophes</h1>
-          <p className="text-sm text-gray-600 ">
+        <div className=" mt-2 ">
+          <h1 className="text-2xl font-bold">Natural Catastrophes</h1>
+          <p className="text-xl text-gray-600 ">
             <strong>Group: </strong>Bantu
           </p>
-          <p className="text-sm text-gray-600 italic font-semibold">
+          <p className="text-xm text-gray-600 italic font-semibold">
             <strong>Group: </strong>Ancient History 3500 - 3000 BCE
           </p>
         </div>
@@ -32,7 +33,7 @@ const RightContainer = () => {
             }`}
             onClick={() => handleTabClick("gallery")}
           />
-          <FaGlobe
+          {/* <FaGlobe
             size={24}
             className={`cursor-pointer p-1 ${
               selectedTab === "list"
@@ -40,7 +41,7 @@ const RightContainer = () => {
                 : "text-gray-600 text-lg"
             }`}
             onClick={() => handleTabClick("list")}
-          />
+          /> */}
           <FaMapMarkerAlt
             size={24}
             className={`cursor-pointer p-1 ${
@@ -56,20 +57,24 @@ const RightContainer = () => {
         {
           // Dynamic Content Area
           selectedTab === "gallery" && (
-            <p className="text-center">
-              The drying of the Sahara around 3000 BCE engendered population
-              redistribution that occurred over centuries. Retreat of rainforest
-              in favor of patches of savannah in parts of West - Central
-              facilitated the immigration of agriculturalists from the north.
-              This process appears to have contributed to the southward spread
-              of Bantu languages from their homeland in the Cameroon grasslands
-              toward eastern and western axes of dispersion corresponding with
-              the main split between West and East Bantu in West Central Africa
-              and the Great Lakes region, respectively. Studies based on
-              lexicostatistics advanced several hypotheses on the subsequent
-              spread and diversification of Bantu all the way to eastern South
-              Africa over an area of more than 11 million km2.
-            </p>
+            <>
+              <p className="text-center">
+                The drying of the Sahara around 3000 BCE engendered population
+                redistribution that occurred over centuries. Retreat of
+                rainforest in favor of patches of savannah in parts of West -
+                Central facilitated the immigration of agriculturalists from the
+                north. This process appears to have contributed to the southward
+                spread of Bantu languages from their homeland in the Cameroon
+                grasslands toward eastern and western axes of dispersion
+                corresponding with the main split between West and East Bantu in
+                West Central Africa and the Great Lakes region, respectively.
+                Studies based on lexicostatistics advanced several hypotheses on
+                the subsequent spread and diversification of Bantu all the way
+                to eastern South Africa over an area of more than 11 million
+                km2.
+              </p>
+              <Gallery />
+            </>
           )
         }
         {selectedTab === "map" && <Map />}
