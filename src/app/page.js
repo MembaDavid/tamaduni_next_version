3908a9c72fd7461cafa40841c261ee3380c87d61
eddiemuhoro <eddiemuhoro@gmail.com>
2024-12-23 19:09:21 +0300

    feat: integrate react-toastify for notifications in layout component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Home from "@/components/home/Home";
 import React from "react";
-import { ToastContainer } from "react-toastify";
 
 function main() {
   return (
     <div className="">
       <Home />
-      <ToastContainer />
     </div>
   );
 }
