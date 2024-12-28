db64d9b0181e62e93fbe9f1339c99153bf500075
eddiemuhoro <eddiemuhoro@gmail.com>
2024-12-28 20:29:31 +0300

    feat: implement data fetching with loading and error handling in DEContent component

--- src/components/views/digitalethnography/Content.js
+++ src/components/views/digitalethnography/Content.js
@@ -2,8 +2,41 @@
 import React from "react";
 import LeftContainer from "./LeftContainer";
 import RightContainer from "./RightContainer";
+import { useEffect } from "react";
+import { base_url } from "@/hooks/urls";
+import { useState } from "react";
+import axios from "axios";
 
 const DEContent = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${base_url}ethnography/search`);
+
+        console.log("data", response.data);
+        setIsError(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading data!</p>;
+  }
+
   return (
     <div className="flex flex-col md:flex-row space-x-6">
       <section className="w-full md:w-1/4">
